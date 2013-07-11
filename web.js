8c001ceef6b2a5f842b514e3c8a3166c4e46c906
Maulik Kataria <maulik.kataria@gmail.com>
2013-07-11 17:14:59 +0000

    Message is displayed from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var buff = fs.readFileSync('index.html');
+    var msg = buff.toString();
+    response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
